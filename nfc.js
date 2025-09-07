7134625856b5440b0825341942c82a00faa21b7c
Francesco Campagnaro <francescocampagnaro1994@gmail.com>
2025-09-07 09:03:20 +0200

    scrivi nfc

--- nfc.js
+++ nfc.js
@@ -1,14 +1,16 @@
 const output = document.getElementById('output');
-const btn = document.getElementById('scanNFC');
 
-btn.addEventListener('click', async () => {
+const btnLeggi = document.getElementById('scanNFC');
+const btnScrivi = document.getElementById('writeNFC');
+
+btnLeggi.addEventListener('click', async () => {
   if ('NDEFReader' in window) {
     try {
       const ndef = new NDEFReader();
       await ndef.scan();
 
       output.textContent = "In attesa di un tag NFC...";
-      
+
       ndef.onreadingerror = () => {
         output.textContent = "Errore nella lettura del tag NFC.";
       };
@@ -28,5 +30,33 @@ btn.addEventListener('click', async () => {
     }
   } else {
     output.textContent = "Web NFC non supportato su questo dispositivo.";
+  }
+});
+function write(data, { timeout } = {}) {
+  return new Promise((resolve, reject) => {
+    const controller = new AbortController();
+    controller.signal.onabort = () =>
+      reject(new Error("Time is up, bailing out!"));
+    setTimeout(() => controller.abort(), timeout);
+
+    ndef.addEventListener(
+      "reading",
+      (event) => {
+        ndef.write(data, { signal: controller.signal }).then(resolve, reject);
+      },
+      { once: true },
+    );
+  });
+}
+
+btnScrivi.addEventListener('click', async () => {
+  await ndef.scan();
+  try {
+    // Let's wait for 5 seconds only.
+    await write("Hello World", { timeout: 10_000 });
+  } catch (err) {
+    console.error("Something went wrong", err);
+  } finally {
+    console.log("We wrote to a tag!");
   }
 });
